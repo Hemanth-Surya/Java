e09cf87c2adfffd431a8f7ec6f4d2f1cb9fc7680
unknown <hemanthsuryavj0@gmail.com>
2021-03-14 21:37:54 +0530

    six

--- Hello.java
+++ Hello.java
@@ -1,30 +1,24 @@
 
-import java.util.Scanner;
-public class Hello
+class Calc
 {
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of rows: ");
- 
-        int rows = sc.nextInt();            
-        for (int i= 0; i<= rows-1 ; i++)
-        {
-            for (int j=0; j <i; j++)
-            {
-                System.out.print(" ");
-            }
-            for (int k=i; k<=rows-1; k++) { System.out.print("*" + " "); } System.out.println(""); } for (int i= rows-1; i>= 0; i--)
-        {
-            for (int j=0; j< i ;j++)
-            {
-                System.out.print(" ");
-            }
-            for (int k=i; k<=rows-1; k++)
-            {
-                System.out.print("*" + " ");
-            }
-            System.out.println("");
-        }
-    }
+	int num1;
+	int num2;
+	int result;
+
+	public void perform()
+	{
+		result = num1 + num2;
+	}
 }
+
+	public class Hello
+	{
+		public static void main(String[] args)
+		{
+			Calc obj = new Calc();
+			obj.num1 = 4;
+			obj.num2 = 6;
+			obj.perform();
+			System.out.println(obj.result);
+		}
+	}
