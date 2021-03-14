14d7c5c8ec1b2ac0f5b4804e428bbb81141af11a
unknown <hemanthsuryavj0@gmail.com>
2021-03-14 14:28:11 +0530

    four

--- Hello.java
+++ Hello.java
@@ -2,19 +2,20 @@ import java.util.Scanner;
 public class Hello {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num1,num2,num3;
-		System.out.println("enter first number");
-		num1 = sc.nextInt();
-		System.out.println("enter second number");
-		num2 = sc.nextInt();
-		System.out.println("enter third number");
-		num3 = sc.nextInt();
-		if(num1 > num2 && num1 >num3)
-			System.out.println("num1 is greater");
-		else if(num2 > num1 && num2 > num3)
-			System.out.println("num2 is greater");
-		else
-			System.out.println("num3 is greater");
-
+		System.out.println("enter number");
+		int num = sc.nextInt();
+		switch(num) {
+			case 1:
+				System.out.println("one");
+				break;
+			case 2:
+				System.out.println("two");
+				break;
+			case 3:
+				System.out.println("three");
+			break;
+			default:
+				System.out.println("none");
+		}
 	}
 }
